c1cc35b88dbafe5b1eb0bd48b53fa755faf1fb0b
PhilippRandau <philipp-randau@web.de>
2023-11-21 17:16:17 +0100

    style and function of add task finished exept data post request

--- src/app/new-task/new-task.component.ts
+++ src/app/new-task/new-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AddTaskDataService } from '../services/add-task-data.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -17,49 +17,69 @@ import { environment } from '../environments/environment.prod';
   encapsulation: ViewEncapsulation.None
 })
 export class NewTaskComponent implements OnInit {
-  categories: any;
-  contacts: any;
-  prio: string = '';
-  subtasks: Array<any> = [];
-  newSubtask: string = '';
-  newCategoryColors: Array<string> = ['#F44336', '#9C27B0', '#3F51B5', '#2196F3', '#00BCD4', '#4CAF50', '#CDDC39', '#FF9800']
-  newCategoryColor: string = '';
-  newCategoryOpen: boolean = false;
-
-  newTaskForm = this.fb.group({
-    title: ['', [Validators.required, Validators.maxLength]],
-    dueDate: ['', [Validators.required]],
-    category: ['', [Validators.required]],
-    assignedTo: [''],
-  });
-
-  get title() { return this.newTaskForm.get('title'); }
-  get dueDate() { return this.newTaskForm.get('dueDate'); }
-  get category() { return this.newTaskForm.get('category'); }
-  get assignedTo() { return this.newTaskForm.get('assignedTo'); }
-
-  newCategoryForm = this.fb.group({
-    newCategory: ['', [Validators.required]],
-  });
-
-  get newCategory() { return this.newCategoryForm.get('newCategory'); }
 
   constructor(
     public dataAddTask: AddTaskDataService,
     private fb: FormBuilder,
     private http: HttpClient
-  ) { }
+  ) { this.loadDataNewTask(); }
 
   ngOnInit(): void {
-    this.loadDataNewTask();
+
   }
 
+  categories: any;
+  contacts: any;
+  creator: any;
+  newSubtasks: Array<any> = [];
+  newCategoryColors: Array<string> = ['#F44336', '#9C27B0', '#3F51B5', '#2196F3', '#00BCD4', '#4CAF50', '#FF9800']
+  // newCategoryColor: string = 'white';
+  newCategoryOpen: boolean = false;
+
+
+
+  newTaskForm = this.fb.group({
+    title: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+    description: [''],
+    prio: [''],
+    dueDate: ['', [Validators.required]],
+    category: ['', [Validators.required,]],
+    assignedTo: [],
+  });
+
+  get title() { return this.newTaskForm.get('title'); }
+  get description() { return this.newTaskForm.get('description'); }
+  get prio() { return this.newTaskForm.get('prio'); }
+  get dueDate() { return this.newTaskForm.get('dueDate'); }
+  get category() { return this.newTaskForm.get('category'); }
+  get assignedTo() { return this.newTaskForm.get('assignedTo'); }
+
+  newCategoryForm = this.fb.group({
+    newCategory: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+    newCategoryColor: ['', [Validators.required]],
+  });
+
+  get newCategory() { return this.newCategoryForm.get('newCategory'); }
+  get newCategoryColor() { return this.newCategoryForm.get('newCategoryColor'); }
+
+  addSubtasksForm = this.fb.group({
+    addSubtask: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+  });
+
+  get addSubtask() { return this.addSubtasksForm.get('addSubtask'); }
+
   async loadDataNewTask() {
+    this.creator = await this.getData('/ownUser/');
     this.categories = await this.getData('/categories/');
     this.contacts = await this.getData('/contacts/');
+
     console.log("contacts: ", this.contacts);
     console.log("categories: ", this.categories);
+    console.log('user logged in: ', this.creator);
+
+    this.assignedTo.setValue([this.creator])
   }
+
 
   getData(endPoint) {
     const url = environment.baseUrl + endPoint;
@@ -67,47 +87,66 @@ export class NewTaskComponent implements OnInit {
   }
 
   createTask() {
-    console.log(this.category.value);
-    console.log(this.assignedTo.value);
+    const newTask = {
+      "section": this.dataAddTask.createTaskInSection,
+      "title": this.title.value,
+      "description": this.description.value,
+      "category": this.category.value,
+      "assigned_to": this.assignedTo.value,
+      "due_date": this.dueDate.value,
+      "prio": this.prio.value,
+      "subtasks": this.newSubtasks,
+      "creator": this.creator
+    }
   }
 
-  test() {
-    console.log(this.dueDate.value);
-
+  setPrio(prioValue) {
+    this.prio.setValue(prioValue);
   }
 
   categoryColorChoosed(color) {
-    this.newCategoryColor = color;
+    this.newCategoryColor.setValue(color);
+  }
+
+  newCategoryTextColor() {
+    return this.newCategoryColor.value === '' ? 'black' : 'white';
   }
 
   async addNewCategory() {
-    debugger
     await this.categories.push(
       {
         "title": this.newCategory.value,
-        "category_color": this.newCategoryColor
+        "category_color": this.newCategoryColor.value
       }
     )
+    this.category.setValue(this.newCategory.value);
     this.backToSelectCategory();
   }
 
   backToSelectCategory() {
     this.newCategoryOpen = false;
-    this.newCategoryColor = '';
     this.clearCategoryInputs();
   }
 
   clearCategoryInputs() {
     this.newCategoryForm.setValue({
-      newCategory: ''
+      newCategory: '',
+      newCategoryColor: ''
     });
   }
 
-  addNewSubtask() {
-    this.subtasks.push(
+  async addNewSubtask() {
+    await this.newSubtasks.push(
       {
-        'title': this.newSubtask,
+        'title': this.addSubtask.value,
         'completed': false
       })
+    this.clearSubtaskInput();
+  }
+
+  clearSubtaskInput() {
+    this.addSubtasksForm.setValue({
+      addSubtask: '',
+    });
   }
 }
